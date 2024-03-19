fb7b6f7a5da131928bee9efe3219a243ca36af3e
RGOTTE <rgotte@ford.com>
2024-03-19 16:16:50 -0300

    Add project files.

---
+++ PruebaTecnicaSysorg/Controllers/Userchoose.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaTecnicaSysorg.Controllers
+{
+    public class Userchoose
+    {
+        public bool CommandChooser() 
+        {
+            var command = Console.ReadLine();                        
+            return true; 
+        }
+    }
+}
---
+++ PruebaTecnicaSysorg/Program.cs
@@ -0,0 +1,14 @@
+﻿using PruebaTecnicaSysorg.Controllers;
+
+class Program
+{
+    static void Main() 
+    {
+        bool lala = true;
+        while (lala) 
+        {
+            lala = new Userchoose().CommandChooser();
+        }
+    }
+    
+}

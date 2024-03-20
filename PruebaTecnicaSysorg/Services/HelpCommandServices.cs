cd7335b826c110974f82d860b80a705adc821d5c
RGOTTE <rgotte@ford.com>
2024-03-19 22:30:41 -0300

    Se agrego la funcionalidad Help.

--- PruebaTecnicaSysorg/Services/HelpCommandServices.cs
+++ PruebaTecnicaSysorg/Services/HelpCommandServices.cs
@@ -10,7 +10,11 @@ namespace PruebaTecnicaSysorg.Services
     {
         public void helpcommand() 
         {
-            Console.WriteLine("-- Se esta utilizando el comando help --");
+            Console.WriteLine("HELP : Se utiliza para ver los comandos posibles y para que se utilizan.");
+            Console.WriteLine("ADD : Se utiliza para agregar los archivos que se necesita para utilizar el 'commit' y realizar la subida.");
+            Console.WriteLine("COMMIT : Se utiliza para guardar los archivos subidos y colocar un mensaje describiendo los cambios.");
+            Console.WriteLine("PUSH : Se utiliza para subir los cambios a la nube.");
+            Console.WriteLine("LOG : Se utiliza para ver el historial de los cambios hechos en la nube.");            
         }
     }
 }

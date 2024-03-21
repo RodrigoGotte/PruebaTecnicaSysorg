e10223cb03d191943dad1bf5706f91ccc10891d2
RGOTTE <rgotte@ford.com>
2024-03-21 00:55:14 -0300

    Se comento program.cs.

--- PruebaTecnicaSysorg/Program.cs
+++ PruebaTecnicaSysorg/Program.cs
@@ -3,16 +3,21 @@ using PruebaTecnicaSysorg.Controllers;
 
 class Program
 {
+    //Create a global variables to keep in memory 
     public static Commit CommitCache = new Commit { Files = new List<Add>(), InsertDate = null, Message = string.Empty };
     public static List<Commit> CommitHistory = new List<Commit>();
 
     static void Main() 
     {
         Console.Clear();
-        bool continuidad = true;
-        while (continuidad) 
+        Console.WriteLine("Welcome to SysorgHUB");
+        Console.WriteLine("Write  command.... ");
+        Console.WriteLine();
+        //This is used for the user to use commands until the user uses the "exit" command.    
+        bool continuity = true;
+        while (continuity) 
         {
-            continuidad = new CommandsControllers().TypeCommand(Console.ReadLine());
+            continuity = new CommandsControllers().TypeCommand(Console.ReadLine());
         }
     }
     

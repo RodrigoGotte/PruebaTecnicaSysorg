8522c43d250bc4b24a3ddb617b8327d53a9f96a5
RGOTTE <rgotte@ford.com>
2024-03-20 21:40:37 -0300

    Se mejoro el log para que se muestre de mejor los archivos.

--- PruebaTecnicaSysorg/Services/LogCommandServices.cs
+++ PruebaTecnicaSysorg/Services/LogCommandServices.cs
@@ -1,4 +1,5 @@
-﻿using PruebaTecnicaSysorg.Clases;
+﻿using Microsoft.Win32;
+using PruebaTecnicaSysorg.Clases;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,17 +9,46 @@ using System.Threading.Tasks;
 namespace PruebaTecnicaSysorg.Services
 {
     public class LogCommandServices
-    {
+    {        
         public void Logcommand(List<Commit> history) 
         {
-            Console.WriteLine("|| FECHA DE CREACION || ARCHIVOS AÑADIDOS || MENSAJES ||");
-            foreach (var commit in history) 
+            int anchoTabla = 80;
+
+            // Caracteres para borde y separadores
+            char bordeHorizontal = '_';
+            char bordeVertical = '|';            
+            char espacio = ' ';
+            int ncommit = 0;
+
+            // Crear StringBuilder
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var registro in history)
             {
-                Console.WriteLine($"||{commit.InsertDate} || {commit.Files[0].NameFile} || {commit.Message} ||");
-                commit.Files.Remove(commit.Files[0]);
-                foreach (var archivo in commit.Files ) 
-                { Console.WriteLine($"                     || {archivo.NameFile}                             ||"); }
+                sb.AppendLine($"{bordeHorizontal.ToString().PadLeft(anchoTabla, bordeHorizontal)}");
+                ncommit++;
+                sb.AppendLine($"{bordeVertical.ToString() + ncommit}° COMMIT");
+                
+                // Agregar encabezado
+                sb.AppendLine($"{bordeHorizontal.ToString().PadLeft(anchoTabla, bordeHorizontal)}");            
+                sb.AppendLine($"{bordeVertical} FECHA CREACION {bordeVertical.ToString().PadLeft(5)} MENSAJES");
+                sb.AppendLine($"{bordeHorizontal.ToString().PadLeft(anchoTabla, bordeHorizontal)}");
+
+                // Agregar filas            
+                sb.AppendLine($"{bordeVertical} {registro.InsertDate.ToString().PadLeft(15, espacio)} {bordeVertical} {registro.Message.PadLeft(1, espacio)}");
+                sb.AppendLine($"{bordeHorizontal.ToString().PadLeft(anchoTabla, bordeHorizontal)}");
+                sb.AppendLine($"{bordeVertical} ARCHIVOS AÑADIDOS  ");
+                sb.AppendLine($"{bordeHorizontal.ToString().PadLeft(anchoTabla, bordeHorizontal)}");
+                foreach (var archivo in registro.Files)
+                {
+                    sb.AppendLine($"{bordeVertical}{archivo.NameFile}");
+                }
+                sb.AppendLine($"{bordeHorizontal.ToString().PadLeft(anchoTabla, bordeHorizontal)}");
+                sb.AppendLine();
+                sb.AppendLine();                
             }
+            // Imprimir la tabla
+            Console.WriteLine(sb.ToString());
         }
     }
 }
